df4d8c233abb34c090b59703dcf3627c229a4022
BIKASH-ai <mbikas213@gmail.com>
2023-10-07 10:59:23 +0530

    complete project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './Home';
-// import ProductInfo from './ProductInfo';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" exact component={Home} />
-//         <Route path="/product/:id" component={ProductInfo} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import Routes from './Routes';
 
